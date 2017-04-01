1a7b21ad2c686a5a3e895716bd5bfe4cfa8a25dc
Niels Steenman <ncsteenman@gmail.com>
2017-04-01 16:03:56 +0200

    Flintboard update

---
+++ src/FlintboardAffiliateProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FlintWebmedia\FlintboardAffiliate;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlintboardAffiliateProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
